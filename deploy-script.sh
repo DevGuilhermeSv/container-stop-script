ca48ab82c2bdc59b8dab88a663096484818de363
Guilherme Silva <49134415+DevGuilhermeSv@users.noreply.github.com>
2025-02-10 16:33:50 -0300

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -3,8 +3,9 @@
 ADMIN_CONTAINER_NAME=$1
 REPO_SLUG=$2
 PORT=$3
+PORT_CONTAINER=$4
 
-if docker run -d -p $PORT:3000 --name $ADMIN_CONTAINER_NAME $REPO_SLUG:latest
+if docker run -d -p $PORT_HOST:$PORT_CONTAINER --name $ADMIN_CONTAINER_NAME $REPO_SLUG:latest
 then echo "Deploy success!"
 else echo "Deploy failed, returnig to last version"
     docker rename '${ADMIN_CONTAINER_NAME}_older' '${ADMIN_CONTAINER_NAME}'
